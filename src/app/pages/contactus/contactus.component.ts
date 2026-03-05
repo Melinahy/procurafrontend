73f007619ea0d8c8457bdc9f82a397034a2aae2e
Melina <sistema.hy@gmail.com>
2026-03-05 18:22:48 -0500

    modificacion página contacto

--- src/app/pages/contactus/contactus.component.ts
+++ src/app/pages/contactus/contactus.component.ts
@@ -1,22 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { NavbarComponent } from "../../components/navbar/navbar/navbar.component";
-import { FooterComponent } from "../../components/footer/footer.component";
-import { ScrollToTopComponent } from "../../components/scroll-to-top/scroll-to-top.component";
+import { NavbarComponent } from '../../components/navbar/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { ScrollToTopComponent } from '../../components/scroll-to-top/scroll-to-top.component';
 
 @Component({
   selector: 'app-contactus',
   imports: [
     CommonModule,
+    FormsModule,
     RouterLink,
     NavbarComponent,
     FooterComponent,
-    ScrollToTopComponent
-],
+    ScrollToTopComponent,
+  ],
   templateUrl: './contactus.component.html',
-  styleUrl: './contactus.component.scss'
+  styleUrl: './contactus.component.scss',
 })
 export class ContactusComponent {
+  form = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
 
+  submitted = false;
+  sending = false;
+  successMsg = '';
+  errorMsg = '';
+
+  onSubmit(): void {
+    this.successMsg = '';
+    this.errorMsg = '';
+
+    if (!this.form.name || !this.form.email || !this.form.message) {
+      this.errorMsg = 'Por favor completa los campos obligatorios.';
+      return;
+    }
+
+    this.sending = true;
+
+    // Simular envio (reemplazar con API real cuando exista)
+    setTimeout(() => {
+      this.sending = false;
+      this.submitted = true;
+      this.successMsg = 'Tu mensaje ha sido enviado correctamente. Te responderemos pronto.';
+      this.form = { name: '', email: '', subject: '', message: '' };
+    }, 1500);
+  }
 }
